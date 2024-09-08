78d238cf5cde388e602692510ca3c375e871ccd0
serega <serega@local.tuf>
2024-09-08 18:26:44 +0300

    DAY-16+17 done

--- app/Models/Job.php
+++ app/Models/Job.php
@@ -4,20 +4,22 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Job extends Model {
     use HasFactory;
 
     protected $table = 'job_listings';
 
-    protected $fillable = ['title', 'salary'];
+    protected $guarded = [];
 
-    public function employer()
+    public function employer(): BelongsTo
     {
         return $this->belongsTo(Employer::class);
     }
 
-    public function tags()
+    public function tags(): BelongsToMany
     {
         return $this->belongsToMany(Tag::class, foreignPivotKey: "job_listing_id");
 
