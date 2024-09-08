e477bca7d65166971ffe7e636457532e23d70484
serega <serega@local.tuf>
2024-09-08 17:29:37 +0300

    DAY-14+15 done

--- database/migrations/2024_08_25_165402_create_tags_table.php
+++ database/migrations/2024_08_25_165402_create_tags_table.php
@@ -20,7 +20,7 @@ return new class extends Migration
         Schema::create('job_tag', function (Blueprint $table) {
             $table->id();
             $table->foreignIdFor(\App\Models\Job::class, 'job_listing_id')->constrained()->cascadeOnDelete();
-            $table->foreignIdFor(\App\Models\Tag::class)->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(\App\Models\Tags::class)->constrained()->cascadeOnDelete();
             $table->timestamps();
         });
     }
