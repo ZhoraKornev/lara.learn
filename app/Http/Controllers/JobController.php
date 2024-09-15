31ee29e665be4b76585b16ea97e628bb2eaa7879
serega <serega@local.tuf>
2024-09-15 15:04:24 +0300

    DAY-19 done CRUD route to resource

---
+++ app/Http/Controllers/JobController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Job;
+use Illuminate\Contracts\View\Factory;
+use Illuminate\Contracts\View\View;
+use Illuminate\Foundation\Application;
+
+class JobController extends Controller
+{
+    public function index(): Factory|Application|View|\Illuminate\Contracts\Foundation\Application
+    {
+        $jobs = Job::with('employer')->latest()->paginate(5);
+
+        return view('jobs.index', [
+            'jobs' => $jobs
+        ]);
+    }
+
+    public function create()
+    {
+        return view('jobs.create');
+
+    }
+
+    public function show(Job $job)
+    {
+        return view('jobs.show', ['job' => $job]);
+
+    }
+
+    public function store()
+    { request()->validate([
+        'title' => 'required|min:3',
+        'salary' => 'required'
+    ]);
+        Job::create([
+            'title' => request('title'),
+            'salary' => request('salary'),
+            'employer_id' => 1
+        ]);
+
+        return redirect('jobs');
+
+    }
+
+    public function edit(Job $job)
+    {
+        return view('jobs.edit', ['job' => $job]);
+
+    }
+    public function update(Job $job)
+    {
+        request()->validate([
+            'title' => 'required|min:3',
+            'salary' => 'required'
+        ]);
+
+        $job->update([
+            'title' => request('title'),
+            'salary' => request('salary'),
+        ]);
+        return redirect('/jobs/'.$job->id);
+
+    }
+    public function destroy(Job $job)
+    {
+        $job->delete();
+
+        return redirect('/jobs');
+
+    }
+}
