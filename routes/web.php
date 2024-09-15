8cba591e3f99d912ffc9be28aab76c3a79488f36
serega <serega@local.tuf>
2024-09-15 15:06:36 +0300

    DAY-19 done CRUD route to resource

--- routes/web.php
+++ routes/web.php
@@ -4,15 +4,11 @@ use App\Http\Controllers\JobController;
 use Illuminate\Support\Facades\Route;
 use App\Models\Job;
 
-Route::get('/', function () {
-    return view('home');
-});
+Route::view('/', 'home');
+Route::view('/contact', 'contact');
+
+Route::resource('jobs', JobController::class);
 
 
-Route::resource('jobs',JobController::class);
-
-Route::get('/contact', function () {
-    return view('contact');
-});
 
 
