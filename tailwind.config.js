3e828e9473e28f8cb5fb8ca7819f180578136a47
serega <serega@local.tuf>
2025-01-02 13:42:14 +0200

    DAY-26 Get Your Build Process in Order

---
+++ tailwind.config.js
@@ -0,0 +1,25 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'laracast': '#F9B23B',
+                'secondary': '#F9B23B',
+                'tertiary': '#F9B23B',
+                'quaternary': '#F9B23B',
+                'quinary': '#F9B23B',
+                'senary': '#F9B23B',
+                'septenary': '#F9B23B',
+                'octonary': '#F9B23B',
+                'nonary': '#F9B23B',
+                'denary': '#F9B23B',
+            },
+        },
+    },
+    plugins: [],
+}
